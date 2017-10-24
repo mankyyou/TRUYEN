29cafafcf0ccd1b21195884b005db1639a643829
thanhnhut <thanhnhut@DESKTOP-3M46G1A>
2017-10-25 00:08:50 +0700

    thay doi timk

--- helloworld/Controllers/TimkiemController.cs
+++ helloworld/Controllers/TimkiemController.cs
@@ -14,7 +14,7 @@ namespace helloworld.Controllers
         {
             return View();
         }
-
+        //tI2M KIE61M THEO TE6N
         public ActionResult ketqua(FormCollection f)
         {
             string tukhoa = f["txtname"];
